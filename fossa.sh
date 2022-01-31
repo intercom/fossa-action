23cf8f05c3c960f4a604c680a7fe9172ccb713c8
Enrico Marugliano <enrico.marugliano@intercom.io>
2022-01-31 17:08:47 +0000

    fix github action path

--- fossa.sh
+++ fossa.sh
@@ -3,7 +3,7 @@ curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/fossas/fossa
 REPO_NAME=${GITHUB_REPOSITORY##*/}
 ~/bin/fossa analyze --project $REPO_NAME
 ANALYZE_EXIT_CODE=$?
-${{ github.action_path }}/publish_dd_metric.sh fossa.analyze.exit_code $ANALYZE_EXIT_CODE $REPO_NAME
+$GITHUB_ACTION_PATH/publish_dd_metric.sh fossa.analyze.exit_code $ANALYZE_EXIT_CODE $REPO_NAME
 if [ $ANALYZE_EXIT_CODE -ne 0 ]; then
     exit 1
 fi
@@ -13,5 +13,5 @@ echo "fossa test exit code: $?"
 FOSSA_EMS_PAYLOAD="{\"repo_name\": \"$REPO_NAME\", \"fossa_issues\": $FOSSA_TEST}"
 echo $FOSSA_EMS_PAYLOAD
 N_ISSUES=`echo $FOSSA_EMS_PAYLOAD | jq -r '.fossa_issues.count'`
-${{ github.action_path }}/publish_dd_metric.sh fossa.num_licensing_issues $N_ISSUES $REPO_NAME
+$GITHUB_ACTION_PATH/publish_dd_metric.sh fossa.num_licensing_issues $N_ISSUES $REPO_NAME
 curl -d "$FOSSA_EMS_PAYLOAD" -H 'Content-Type: application/json' -H 'X-Auth-Token: $FOSSA_EVENT_RECEIVER_TOKEN' 'https://event-management-system.internal.intercom.io/fossa_event_receiver?event_type=fossa_licensing_issues'
