fd25c66d4382b8fceba9b2c6166ab75b2d8eb4b5
Enrico Marugliano <enrico.marugliano@intercom.io>
2022-01-31 17:31:21 +0000

    test failed analyze

--- fossa.sh
+++ fossa.sh
@@ -1,17 +1,16 @@
 set +e
 curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/fossas/fossa-cli/master/install-latest.sh | bash -s -- -b ~/bin
 REPO_NAME=${GITHUB_REPOSITORY##*/}
-~/bin/fossa analyze --project $REPO_NAME
+
+~/bin/fossa analyz --project $REPO_NAME
 ANALYZE_EXIT_CODE=$?
 $GITHUB_ACTION_PATH/publish_dd_metric.sh fossa.analyze.exit_code $ANALYZE_EXIT_CODE $REPO_NAME
-if [ $ANALYZE_EXIT_CODE -ne 0 ]; then
-    exit 1
-fi
+if [ $ANALYZE_EXIT_CODE -ne 0 ]; then exit 1; fi
+
 FOSSA_TEST=`~/bin/fossa test --json --project $REPO_NAME || exit 0`
-echo "fossa test exit code: $?"
 [ -z "$FOSSA_TEST" ] && FOSSA_TEST={}
+
 FOSSA_EMS_PAYLOAD="{\"repo_name\": \"$REPO_NAME\", \"fossa_issues\": $FOSSA_TEST}"
-echo $FOSSA_EMS_PAYLOAD
 N_ISSUES=`echo $FOSSA_EMS_PAYLOAD | jq -r '.fossa_issues.count'`
 $GITHUB_ACTION_PATH/publish_dd_metric.sh fossa.num_licensing_issues $N_ISSUES $REPO_NAME
-curl -d "$FOSSA_EMS_PAYLOAD" -H 'Content-Type: application/json' -H 'X-Auth-Token: $FOSSA_EVENT_RECEIVER_TOKEN' 'https://event-management-system.internal.intercom.io/fossa_event_receiver?event_type=fossa_licensing_issues'
+curl -sS -d "$FOSSA_EMS_PAYLOAD" -H 'Content-Type: application/json' -H 'X-Auth-Token: $FOSSA_EVENT_RECEIVER_TOKEN' 'https://event-management-system.internal.intercom.io/fossa_event_receiver?event_type=fossa_licensing_issues'
--- publish_dd_metric.sh
+++ publish_dd_metric.sh
@@ -2,14 +2,14 @@ METRIC_NAME=$1
 METRIC_VALUE=$2
 HOST=$3
 
-if [ -z ${DATADOG_API_KEY} ]; then
-  echo "No Datadog API key provided.. observability disabled"
+if [[ -z ${DATADOG_API_KEY} || -z ${METRIC_NAME} || -z ${METRIC_VALUE} || -z ${HOST} ]]; then
+  echo "One or more required variables are missing: DATADOG_API_KEY, METRIC_NAME, METRIC_VALUE, HOST"
   exit 0
 fi
 
 export NOW="$(date +%s)"
 
-curl -X POST "https://api.datadoghq.com/api/v1/series" \
+curl -sS -X POST "https://api.datadoghq.com/api/v1/series" \
 -H "Content-Type: text/json" \
 -H "DD-API-KEY: ${DATADOG_API_KEY}" \
 -d @- << EOF
